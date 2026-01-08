4e98551595973344a8dddd8f721262990d7f220f
alegian <alegiannx@gmail.com>
2026-01-08 15:22:16 +0200

    start fixing datagen

--- src/main/java/me/alegian/thavma/impl/common/aspect/AspectMap.kt
+++ src/main/java/me/alegian/thavma/impl/common/aspect/AspectMap.kt
@@ -2,11 +2,14 @@ package me.alegian.thavma.impl.common.aspect
 
 import com.mojang.serialization.Codec
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.DATAGEN_PRIMALS
+import net.minecraft.core.Holder
 import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
 import java.util.*
 import java.util.function.Consumer
 import java.util.function.Supplier
+import kotlin.math.round
+import kotlin.math.roundToInt
 
 /**
  * Immutable.
@@ -33,6 +36,8 @@ class AspectMap(map: Map<Aspect, Int> = LinkedHashMap()) : Iterable<AspectStack>
     return builder().copyOf(this).add(aspect, amount).build()
   }
 
+  fun add(aspectHolder: Holder<Aspect>, amount: Int) = add(aspectHolder.value(), amount)
+
   fun add(other: AspectMap): AspectMap {
     val builder = builder().copyOf(this)
     other.forEach(Consumer { aspectStack: AspectStack -> builder.add(aspectStack) })
@@ -42,6 +47,8 @@ class AspectMap(map: Map<Aspect, Int> = LinkedHashMap()) : Iterable<AspectStack>
   fun subtract(aspect: Aspect, amount: Int): AspectMap {
     return builder().copyOf(this).subtract(aspect, amount).build()
   }
+
+  fun subtract(aspectHolder: Holder<Aspect>, amount: Int) = subtract(aspectHolder.value(), amount)
 
   fun subtract(other: AspectMap): AspectMap {
     val builder = builder().copyOf(this)
@@ -139,7 +146,7 @@ class AspectMap(map: Map<Aspect, Int> = LinkedHashMap()) : Iterable<AspectStack>
     }
 
     fun scale(multiplier: Number): Builder {
-      map.forEach { (k: Aspect, v: Int) -> map[k] = (v * multiplier.toDouble()).toInt() }
+      map.forEach { (k: Aspect, v: Int) -> map[k] = round(v * multiplier.toDouble()).toInt() }
       map = LinkedHashMap(map.filterValues { it > 0 })
       return this
     }
--- src/main/java/me/alegian/thavma/impl/init/data/providers/aspects/MineralAspects.kt
+++ src/main/java/me/alegian/thavma/impl/init/data/providers/aspects/MineralAspects.kt
@@ -1,5 +1,6 @@
 package me.alegian.thavma.impl.init.data.providers.aspects
 
+import me.alegian.thavma.impl.common.aspect.AspectMap
 import me.alegian.thavma.impl.init.data.providers.T7DataMapProvider
 import me.alegian.thavma.impl.init.data.providers.item
 import me.alegian.thavma.impl.init.registries.T7DataMaps
@@ -10,44 +11,71 @@ import me.alegian.thavma.impl.init.registries.deferred.T7Blocks.INFUSED_STONES
 import me.alegian.thavma.impl.init.registries.deferred.T7Items
 import me.alegian.thavma.impl.init.registries.deferred.T7Items.SHARDS
 import net.minecraft.core.HolderLookup
+import net.minecraft.resources.ResourceKey
 import net.minecraft.tags.ItemTags
+import net.minecraft.tags.TagKey
 import net.minecraft.world.item.Items
 import net.minecraft.world.level.block.Blocks
 import net.neoforged.neoforge.common.Tags
+import net.neoforged.neoforge.common.data.DataMapProvider
+import java.util.function.UnaryOperator
 
 object MineralAspects {
+  val coal = AspectGen().mutate {
+    it.add(Aspects.IGNIS, 2)
+      .add(Aspects.TERRA, 2)
+  }
+  val coalOre = coal.mutate(Mutations.ORE)
+  val coalBlock = coal.mutate(Mutations.STORAGE_BLOCK_9)
+
+  val copper = AspectGen().mutate {
+    it.add(Aspects.METALLUM, 4)
+  }
+  val rawCopper = copper.mutate(Mutations.RAW)
+  val copperOre = copper.mutate(Mutations.ORE)
+  val copperBlock = copper.mutate(Mutations.STORAGE_BLOCK_9)
+  val rawCopperBlock = rawCopper.mutate(Mutations.STORAGE_BLOCK_9)
+
+  val iron = AspectGen().mutate {
+    it.add(Aspects.METALLUM, 8)
+  }
+  val rawIron = iron.mutate(Mutations.RAW)
+  val ironOre = iron.mutate(Mutations.ORE)
+  val ironBlock = iron.mutate(Mutations.STORAGE_BLOCK_9)
+  val rawIronBlock = rawIron.mutate(Mutations.STORAGE_BLOCK_9)
+
+  val gold = AspectGen().mutate {
+    it.add(Aspects.METALLUM, 4)
+  }
+  val rawGold = gold.mutate(Mutations.RAW)
+  val goldOre = gold.mutate(Mutations.ORE)
+  val goldBlock = gold.mutate(Mutations.STORAGE_BLOCK_9)
+  val rawGoldBlock = rawGold.mutate(Mutations.STORAGE_BLOCK_9)
+
   fun gather(datamapProvider: T7DataMapProvider, lookupProvider: HolderLookup.Provider) {
     datamapProvider.builder(T7DataMaps.AspectContent.ITEM).run {
-      item(Tags.Items.ORES_COAL) {
-        it.add(Aspects.IGNIS, 2)
-          .add(Aspects.TERRA, 2)
-      }
-      item(Tags.Items.STORAGE_BLOCKS_COAL) {
-        it.add(Aspects.IGNIS, 18)
-          .add(Aspects.TERRA, 18)
-      }
-      item(Tags.Items.ORES_COPPER) {
-        it.add(Aspects.METALLUM, 4)
-          .add(Aspects.TERRA, 4)
-      }
-      item(Tags.Items.STORAGE_BLOCKS_RAW_COPPER) {
-        it.add(Aspects.METALLUM, 18)
-          .add(Aspects.TERRA, 18)
-      }
-      item(Tags.Items.STORAGE_BLOCKS_COPPER) {
-        it.add(Aspects.METALLUM, 36)
-      }
-      item(Tags.Items.ORES_IRON) {
-        it.add(Aspects.METALLUM, 8)
-          .add(Aspects.TERRA, 4)
-      }
-      item(Tags.Items.STORAGE_BLOCKS_RAW_IRON) {
-        it.add(Aspects.METALLUM, 36)
-          .add(Aspects.TERRA, 18)
-      }
-      item(Tags.Items.STORAGE_BLOCKS_IRON) {
-        it.add(Aspects.METALLUM, 72)
-      }
+      coal.save(this, ItemTags.COALS)
+      coalOre.save(this, Tags.Items.ORES_COAL)
+      coalBlock.save(this, Tags.Items.STORAGE_BLOCKS_COAL)
+
+      copper.save(this, Tags.Items.INGOTS_COPPER)
+      rawCopper.save(this, Tags.Items.RAW_MATERIALS_COPPER)
+      copperOre.save(this, Tags.Items.ORES_COPPER)
+      copperBlock.save(this, Tags.Items.STORAGE_BLOCKS_COPPER)
+      rawCopperBlock.save(this, Tags.Items.STORAGE_BLOCKS_RAW_COPPER)
+
+      iron.save(this, Tags.Items.INGOTS_IRON)
+      rawIron.save(this, Tags.Items.RAW_MATERIALS_IRON)
+      ironOre.save(this, Tags.Items.ORES_IRON)
+      ironBlock.save(this, Tags.Items.STORAGE_BLOCKS_IRON)
+      rawIronBlock.save(this, Tags.Items.STORAGE_BLOCKS_RAW_IRON)
+
+      gold.save(this, Tags.Items.INGOTS_GOLD)
+      rawGold.save(this, Tags.Items.RAW_MATERIALS_GOLD)
+      goldOre.save(this, Tags.Items.ORES_GOLD)
+      goldBlock.save(this, Tags.Items.STORAGE_BLOCKS_GOLD)
+      rawGoldBlock.save(this, Tags.Items.STORAGE_BLOCKS_RAW_GOLD)
+
       item(T7Blocks.THAVMITE_BLOCK) {
         it.add(Aspects.METALLUM, 72)
           .add(Aspects.AETHER, 36)
@@ -55,17 +83,6 @@ object MineralAspects {
       item(T7Blocks.ORICHALCUM_BLOCK) {
         it.add(Aspects.METALLUM, 36)
           .add(Aspects.AETHER, 36)
-      }
-      item(Tags.Items.ORES_GOLD) {
-        it.add(Aspects.METALLUM, 4)
-          .add(Aspects.TERRA, 4)
-      }
-      item(Tags.Items.STORAGE_BLOCKS_RAW_GOLD) {
-        it.add(Aspects.METALLUM, 18)
-          .add(Aspects.TERRA, 18)
-      }
-      item(Tags.Items.STORAGE_BLOCKS_GOLD) {
-        it.add(Aspects.METALLUM, 36)
       }
       item(Tags.Items.ORES_DIAMOND) {
         it.add(Aspects.VITREUS, 8)
@@ -129,27 +146,7 @@ object MineralAspects {
           it.add(infusedBlock.get().getAspect(), 4)
         }
       }
-    }
 
-    datamapProvider.builder(T7DataMaps.AspectContent.ITEM).run {
-      item(ItemTags.COALS) {
-        it.add(Aspects.IGNIS, 2)
-        it.add(Aspects.TERRA, 2)
-      }
-      item(Tags.Items.RAW_MATERIALS_COPPER) {
-        it.add(Aspects.METALLUM, 2)
-          .add(Aspects.TERRA, 2)
-      }
-      item(Tags.Items.INGOTS_COPPER) {
-        it.add(Aspects.METALLUM, 4)
-      }
-      item(Tags.Items.RAW_MATERIALS_IRON) {
-        it.add(Aspects.METALLUM, 4)
-          .add(Aspects.TERRA, 2)
-      }
-      item(Tags.Items.INGOTS_IRON) {
-        it.add(Aspects.METALLUM, 8)
-      }
       item(T7Items.THAVMITE_INGOT) {
         it.add(Aspects.METALLUM, 8)
           .add(Aspects.AETHER, 4)
@@ -157,13 +154,6 @@ object MineralAspects {
       item(T7Items.ORICHALCUM_INGOT) {
         it.add(Aspects.METALLUM, 4)
           .add(Aspects.INSTRUMENTUM, 4)
-      }
-      item(Tags.Items.RAW_MATERIALS_GOLD) {
-        it.add(Aspects.METALLUM, 2)
-          .add(Aspects.TERRA, 2)
-      }
-      item(Tags.Items.INGOTS_GOLD) {
-        it.add(Aspects.METALLUM, 4)
       }
       item(Tags.Items.GEMS_DIAMOND) {
         it.add(Aspects.VITREUS, 8)
@@ -209,6 +199,36 @@ object MineralAspects {
       item(Tags.Items.NUGGETS) {
         it.add(Aspects.TERRA, 1)
       }
+    }
+  }
+
+  class AspectGen(private var aspects: AspectMap) {
+    constructor() : this(AspectMap())
+
+    fun mutate(mutation: UnaryOperator<AspectMap>) =
+      AspectGen(mutation.apply(aspects))
+
+    fun <T> save(builder: DataMapProvider.Builder<AspectMap, T>, tag: TagKey<T>){
+      builder.add(tag, aspects, false)
+    }
+
+    fun <T> save(builder: DataMapProvider.Builder<AspectMap, T>, key: ResourceKey<T>){
+      builder.add(key, aspects, false)
+    }
+  }
+
+  object Mutations {
+    val ORE = { it: AspectMap ->
+      it.add(Aspects.TERRA, 2)
+    }
+    val RAW = { it: AspectMap ->
+      it.scale(0.5).add(Aspects.TERRA, 2)
+    }
+    val STORAGE_BLOCK_4 = { it: AspectMap ->
+      it.scale(4)
+    }
+    val STORAGE_BLOCK_9 = { it: AspectMap ->
+      it.scale(9)
     }
   }
 }
